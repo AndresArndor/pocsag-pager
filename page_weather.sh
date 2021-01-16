b8e964f572029beac53c4a8e89444d90e01133e7
AndresArndor <naiperlit347@gmail.com>
2021-01-17 00:43:07 +0400

    [PAGER]: playing around

--- page_weather.sh
+++ page_weather.sh
@@ -7,15 +7,15 @@ mkdir -p /tmp/pager
 wget ${weather_station_link} -O /tmp/pager/weather_station_report
 
 temperature=`cat /tmp/pager/weather_station_report | grep -in "Latest recived weather reports" -A 40 | grep 357 | awk -F'[ ,]+' '{print $2}' | cut -f1 -d"&"`
-echo $temperature
+export $temperature
 
 pressure=`cat /tmp/pager/weather_station_report | grep -in "Latest recived weather reports" -A 40 | grep 363 | awk -F'[ ,]+' '{print $2}'`
-echo $pressure
+export $pressure
 
 wind_gust=`cat /tmp/pager/weather_station_report | grep -in "Latest recived weather reports" -A 40 | grep 371 | awk -F'[ ,]+' '{print $5}'`
-echo $wind_gust
+export $wind_gust
 
 wind=`cat /tmp/pager/weather_station_report | grep -in "Latest recived weather reports" -A 40 | grep 373 | awk -F'[ ,]+' '{print $2}'`
-echo $wind
+export $wind
 
 sudo RemoteCommand 7642 page 1234567 "R4CHD, weather: T: ${temperature}, P: ${pressure}, W: ${wind_gust}"
