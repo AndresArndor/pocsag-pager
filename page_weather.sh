572a5f5ed545d08e338185a616af22bbe739a960
AndresArndor <naiperlit347@gmail.com>
2021-01-20 17:03:11 +0400

    [PAGER]: add humidity

--- page_weather.sh
+++ page_weather.sh
@@ -9,6 +9,9 @@ wget ${weather_station_link} -O /tmp/pager/weather_station_report
 temperature=`cat /tmp/pager/weather_station_report | grep -in "Latest recived weather reports" -A 40 | grep 357 | awk -F'[ ,]+' '{print $2}' | cut -f1 -d"&"`
 export $temperature
 
+humidity=`cat /tmp/pager/weather_station_report | grep -in "Latest recived weather reports" -A 40 | grep 360 | awk -F'[ ,]+' '{print $2}' | cut -f1 -d"&"`
+export $humidity
+
 pressure=`cat /tmp/pager/weather_station_report | grep -in "Latest recived weather reports" -A 40 | grep 363 | awk -F'[ ,]+' '{print $2}'`
 export $pressure
 
@@ -18,4 +21,4 @@ export $wind_gust
 wind=`cat /tmp/pager/weather_station_report | grep -in "Latest recived weather reports" -A 40 | grep 373 | awk -F'[ ,]+' '{print $2}'`
 export $wind
 
-sudo RemoteCommand 7642 page 1234567 "R4CHD, weather: T: ${temperature}, P: ${pressure}, W: ${wind_gust}"
+sudo RemoteCommand 7642 page 1234567 "R4CHD, weather: T: ${temperature}, H: ${humidity}, P: ${pressure}, W: ${wind_gust}"
