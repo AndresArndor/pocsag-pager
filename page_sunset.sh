596045831977717cb446b38543917db0e36a1862
AndresArndor <naiperlit347@gmail.com>
2021-02-02 19:56:21 +0400

    [PAGER]: add sunset

--- page_sunset.sh
+++ page_sunset.sh
@@ -8,4 +8,4 @@ wget ${sun_rise_set_link} -O /tmp/pager/sun_report
 
 sunset_time=`cat /tmp/pager/sun_report | grep -in "Восход Солнца" -A 21 | grep 320 | awk -F'[ ,]+' '{print $2}' | awk -F'[><]+' '{print $2}' | cut -f1 -d"&"`
 
-sudo RemoteCommand 7642 page 1234567 "R4CHD, Sunset today: ${sunset_today}"
+sudo RemoteCommand 7642 page 1234567 "R4CHD, Sunset today: ${sunset_time}"
