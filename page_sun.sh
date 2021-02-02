6a273a0e68f7b2456e5d7a56fb339b77f9c5fac2
AndresArndor <naiperlit347@gmail.com>
2021-02-02 19:49:59 +0400

    [PAGER]: add sunset

---
+++ page_sun.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sun_rise_set_link="https://dateandtime.info/ru/citysunrisesunset.php?id=498677"
+
+mkdir -p /tmp/pager
+
+wget ${sun_rise_set_link} -O /tmp/pager/sun_report
+
+sunset_time=`cat /tmp/pager/sun_report | grep -in "Восход Солнца" -A 21 | grep 320 | awk -F'[ ,]+' '{print $2}' | awk -F'[><]+' '{print $2}' | cut -f1 -d"&"`
+
+sudo RemoteCommand 7642 page 1234567 "R4CHD, Sunset today: ${sunset_today}"
